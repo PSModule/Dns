c6d774c8d21f0c254fbe7bd7faad0bb934b0be46
Marius Storhaug <marstor@hotmail.com>
2025-08-27 12:00:08 +0200

    🌟 [Major]: Add function for resolving DNS hosts (#3)
    ## Description
    
    This pull request adds function for resolving DNS hosts by using the
    base .net class `[System.Net.Dns]`.
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [ ] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [x] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

---
+++ src/functions/public/Resolve-DnsHost.ps1
@@ -0,0 +1,34 @@
+﻿function Resolve-DnsHost {
+    <#
+        .SYNOPSIS
+        Resolves a hostname to an IP address.
+
+        .DESCRIPTION
+        This function resolves a hostname to an IP address using the System.Net.Dns class.
+
+        .EXAMPLE
+        Resolve-DnsHost -HostName 'google.com'
+
+        .OUTPUTS
+        [DnsHost]
+    #>
+    [OutputType([DnsHost])]
+    [CmdletBinding()]
+    param (
+        # The name of the host to resolve
+        [Parameter(Mandatory)]
+        [string] $Name,
+
+        # The address family to use for the DNS resolution
+        [Parameter()]
+        [System.Net.Sockets.AddressFamily] $AddressFamily = 'Unspecified'
+    )
+
+    try {
+        $entry = [System.Net.Dns]::GetHostEntry($Name, $AddressFamily)
+        return [DnsHost]::new($entry.HostName, $entry.Aliases, $entry.AddressList)
+    } catch {
+        Write-Debug "Failed to resolve DNS for [$Name]"
+        Write-Debug $_
+    }
+}
---
+++ tests/Dns.Tests.ps1
@@ -0,0 +1,53 @@
+﻿#Requires -Modules @{ ModuleName = 'Pester'; RequiredVersion = '5.7.1' }
+
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSUseDeclaredVarsMoreThanAssignments', '',
+    Justification = 'Pester grouping syntax: known issue.'
+)]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSAvoidUsingConvertToSecureStringWithPlainText', '',
+    Justification = 'Used to create a secure string for testing.'
+)]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSAvoidUsingWriteHost', '',
+    Justification = 'Log outputs to GitHub Actions logs.'
+)]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSAvoidLongLines', '',
+    Justification = 'Long test descriptions and skip switches'
+)]
+[CmdletBinding()]
+param()
+
+Describe 'Dns' {
+
+    Context 'Resolve-DnsHost' {
+        It 'Test record <Name> - should exist <expected>' -ForEach @(
+            @{
+                Name     = 'google.com'
+                Expected = $true
+            },
+            @{
+                Name     = 'example.com'
+                Expected = $true
+            },
+            @{
+                Name     = 'nonexistent.example'
+                Expected = $false
+            }
+        ) {
+            $result = Resolve-DnsHost -Name $Name -ErrorAction Stop
+            LogGroup 'Results' {
+                Write-Host "$($result | Format-Table -AutoSize | Out-String)"
+            }
+            if ($Expected) {
+                $result | Should -Not -BeNullOrEmpty
+                $result | Should -BeOfType [DnsHost]
+                $result.Name | Should -Be $Name
+                $result.AddressList.Count | Should -BeGreaterThan 0
+            } else {
+                $result | Should -BeNullOrEmpty
+            }
+        }
+    }
+}
