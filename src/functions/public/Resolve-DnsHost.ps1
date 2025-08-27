225597cf85c8037ceb29092c16e703be5acc904b
Marius Storhaug <marstor@hotmail.com>
2025-08-27 10:57:28 +0200

    Remove deprecated files and functions, including example scripts, configuration, and type definitions, to streamline the module.

---
+++ src/functions/public/Resolve-DnsHost.ps1
@@ -0,0 +1,25 @@
+﻿function Resolve-DnsHost {
+    <#
+        .SYNOPSIS
+        Resolves a hostname to an IP address.
+
+        .DESCRIPTION
+        This function resolves a hostname to an IP address using the System.Net.Dns class.
+
+        .EXAMPLE
+        Resolve-Host -HostName 'google.com'
+
+        .OUTPUTS
+        [System.Net.IPHostEntry](https://learn.microsoft.com/en-us/dotnet/api/system.net.iphostentry?view=net-8.0)
+    #>
+    [OutputType([System.Net.IPHostEntry])]
+    [CmdletBinding()]
+    param (
+        # The hostname to resolve
+        [Parameter(Mandatory)]
+        [Alias('IP', 'Address', 'Host', 'IPAddress', 'Destination')]
+        [string] $HostName
+    )
+
+    [System.Net.Dns]::Resolve($HostName)
+}
---
+++ tests/Dns.Tests.ps1
@@ -0,0 +1,33 @@
+﻿# filepath: c:\Repos\GitHub\PSModule\Module\Dns\tests\Dns.Tests.ps1
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSReviewUnusedParameter', '',
+    Justification = 'Required for Pester tests'
+)]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSUseDeclaredVarsMoreThanAssignments', '',
+    Justification = 'Required for Pester tests'
+)]
+[CmdletBinding()]
+param()
+
+Describe 'Dns' {
+
+    Describe 'Resolve-DnsHost' {
+        Context 'Existing record' {
+            It 'returns at least one record for example.com' {
+                $result = $null
+                { $script:result = Resolve-DnsHost -Name 'example.com' -ErrorAction Stop } | Should -Not -Throw
+                $result | Should -Not -BeNullOrEmpty
+                LogGroup 'Results' {
+                    Write-Host "$($result | Format-Table -AutoSize | Out-String)"
+                }
+            }
+        }
+
+        Context 'Non-existing record' {
+            It 'returns no records or throws for a non-existent host' {
+                { Resolve-DnsHost -Name 'no-such-host.invalid' -ErrorAction Stop } | Should -Not -Throw
+            }
+        }
+    }
+}
