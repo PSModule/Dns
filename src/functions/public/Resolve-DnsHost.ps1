d56819406e8e1699b528e72cf28d77389299cecd
Marius Storhaug <marstor@hotmail.com>
2025-08-27 11:09:22 +0200

    Fix example and implementation in Resolve-DnsHost function for consistency

--- src/functions/public/Resolve-DnsHost.ps1
+++ src/functions/public/Resolve-DnsHost.ps1
@@ -7,7 +7,7 @@
         This function resolves a hostname to an IP address using the System.Net.Dns class.
 
         .EXAMPLE
-        Resolve-Host -HostName 'google.com'
+        Resolve-DnsHost -HostName 'google.com'
 
         .OUTPUTS
         [System.Net.IPHostEntry](https://learn.microsoft.com/en-us/dotnet/api/system.net.iphostentry?view=net-8.0)
@@ -20,5 +20,5 @@
         [string] $Name
     )
 
-    [System.Net.Dns]::Resolve($Name)
+    [System.Net.Dns]::GetHostEntry($Name)
 }
